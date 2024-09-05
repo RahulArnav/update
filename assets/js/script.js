a45c0653bf4dfa8aeaf02e0ca217333c8a1aa803
RahulArnav <profrahulgupta.com>
2024-09-05 08:04:37 +0530

    ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,7 +73,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Software Tester", "Automation Tester", "REST API Tester", "Test Framework Designer", "Performance Tester"],
+    strings: ["Web Application Tester", "Automation Tester", "Mobile Application Tester","REST API Tester", "Test Framework Designer", "Performance Tester"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
